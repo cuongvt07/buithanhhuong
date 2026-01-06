35444312d0f63f62889cc600772e73cc3e733f8d
cuong pham <cuongdtnt109@gmail.com>
2026-01-06 15:58:20 +0700

    update background main

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: '/buithanhhuong/', // ✅ CHUẨN cho GitHub Pages repo
+    base: './',
 
     server: {
       port: 3000,
